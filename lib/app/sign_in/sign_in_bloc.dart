fed877350f6ff3270ec819714de8e3af5120fd17
hesam <hesam.kamalan@gmail.com>
2020-05-21 16:56:54 -0600

    Bloc added to the SignIn screen.

--- lib/app/sign_in/sign_in_bloc.dart
+++ lib/app/sign_in/sign_in_bloc.dart
@@ -1,6 +1,13 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
+import 'package:udemy/services/auth/auth.dart';
+
 class SignInBloc {
+  SignInBloc({@required this.auth});
+
+  final AuthBase auth;
+
   final StreamController<bool> _isLoadingController = StreamController<bool>();
 
   Stream<bool> get isLoadingStream => _isLoadingController.stream;
@@ -9,5 +16,23 @@ class SignInBloc {
     _isLoadingController.close();
   }
 
-  void setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
+  void _setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
+
+  Future<User> signInAnonymously() async =>
+      await _signIn(auth.signInAnonymously);
+
+  Future<User> signInWithGoogle() async => await _signIn(auth.signInWithGoogle);
+
+  Future<User> signInWithFacebook() async =>
+      await _signIn(auth.signInWithFacebook);
+
+  Future<User> _signIn(Future<User> Function() signInMethod) async {
+    try {
+      _setIsLoading(true);
+      return await signInMethod();
+    } catch (e) {
+      _setIsLoading(false);
+      rethrow;
+    }
+  }
 }
